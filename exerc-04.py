c4f51d022408a2028e125993fc456bc2188c4127
Emily da Costa <88409795+Emilyscs@users.noreply.github.com>
2021-08-04 17:31:49 -0300

    Update exerc-04.py

--- exerc-04.py
+++ exerc-04.py
@@ -1,5 +1,4 @@
-#Faça um programa que pergunte quanto você ganha por hora e o número
-#de horas trabalhadas no mês. Calcule e mostre o total de seu salário no referido mês.
+#Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total de seu salário no referido mês.
 
 horas_mes = int(input("Informe a quantidade de horas trabalhadas no mês: "))
 sal_hora = int(input("Informe o seu ganho por hora: "))
